436e9a609b82a674c6b0b5d2b84135f5340b8bf6
Roy Tanck <roy.tanck@gmail.com>
2021-12-03 19:17:00 +0100

    Shuffled the code a bit so the functions that do the heavy lifting are at the top.

--- wallpaper.js
+++ wallpaper.js
@@ -17,51 +17,8 @@ const keysReference = [
 ];
 
 /**
- * Encodes the generated values to Base64 to be sharable. It takes an object in
- * parameters, sort the keys based on "keysReference" to make sure the encoding
- * and decoding step are consistent, join the values and encode it in base64.
+ * Generate a set of randomized values to create a wallpaper from.
  */
-function encodeValues( values ){
-	const joinedValues = keysReference.reduce( ( str, key, index ) =>
-		str + values[ key ] + ( index < keysReference.length - 1 ? ',' : '' )
-	, '');
-	return window.btoa( joinedValues );
-}
-
-/**
- * Parses an base64 encoded string to get all the variables for a wallpaper.
- * See "encodeValues" for the content.
- */
-function getValuesFromBase64( encoded ){
-	const data = window.atob( encoded );
-	// Quick validation for malformed "share" query parameter
-	if( data.match(/,/g).length !== 10 ){
-		return null;
-	}
-	// Should have sorted keys based on "keysReference"
-	const values = data.split( ',' );
-	const mappedValues = keysReference.reduce( ( obj, key, index ) => ({
-		...obj,
-		[ key ]: Number( values[ index ] )
-	}), {});
-	return mappedValues;
-}
-
-/**
- * It ill check the "share" query parameter to retrieve variables used to generate
- * a wallpaper. If present, it will parse it and return all the decoded variables.
- * Otherwise, it will return null
- */
-function getDataFromUrl(){
-	const params = new URLSearchParams( window.location.search );
-	// Set from the "share" link, in "setLinks" function
-	const share = params.get( 'share' );
-	if( !share ){
-		return null;
-	}
-	return getValuesFromBase64( share );
-}
-
 function generateValues( width ){
 	// line segments (either few, or fluent lines (200))
 	const segments = ( Math.random() < 0.5 ) ? 1 + Math.floor( 9 * Math.random() ) : 200;
@@ -93,20 +50,13 @@ function generateValues( width ){
 		offsetIncrement: width/20 + (width/10) * Math.random(),
 		sat: 10 + ( 30 * Math.random() ),
 		light,
-		lightIncrement: lightIncrement
+		lightIncrement
 	};
 }
 
-function setLinks( imageData, encodedValues ){
-	// Allows to download the image, whatever the device
-	const dlLink = document.querySelector( 'a#download' );
-	dlLink.href = imageData;
-	// Allows to share a given wallpaper, generating the exact same one
-	// We could push the query parameter automatically with "window.history.pushState"
-	const shareLink = document.querySelector( 'a#share' );
-	shareLink.href = "?share=" + encodedValues;
-}
-
+/**
+ * Draw the wallpaper onto the canvas, using values from the URL or a fresh set.
+ */
 function draw(){
 	const canvas = document.querySelector( 'canvas' );
 	const ctx = canvas.getContext( '2d' );
@@ -114,7 +64,6 @@ function draw(){
 	const height = 2160;
 
 	const values = getDataFromUrl() ?? generateValues( width );
-	console.log( values );
 	const {
 		segments,
 		layers,
@@ -155,4 +104,63 @@ function draw(){
 	}
 
 	setLinks( canvas.toDataURL(), encodeValues(values) );
+}
+
+/**
+ * Set the href for the download and share links
+ */
+ function setLinks( imageData, encodedValues ){
+	// Allows to download the image, whatever the device
+	const dlLink = document.querySelector( 'a#download' );
+	dlLink.href = imageData;
+	// Allows to share a given wallpaper, generating the exact same one
+	// We could push the query parameter automatically with "window.history.pushState"
+	const shareLink = document.querySelector( 'a#share' );
+	shareLink.href = "?share=" + encodedValues;
+}
+
+/**
+ * Encodes the generated values to Base64 to be sharable. It takes an object in
+ * parameters, sort the keys based on "keysReference" to make sure the encoding
+ * and decoding step are consistent, join the values and encode it in base64.
+ */
+ function encodeValues( values ){
+	const joinedValues = keysReference.reduce( ( str, key, index ) =>
+		str + values[ key ] + ( index < keysReference.length - 1 ? ',' : '' )
+	, '');
+	return window.btoa( joinedValues );
+}
+
+/**
+ * Parses an base64 encoded string to get all the variables for a wallpaper.
+ * See "encodeValues" for the content.
+ */
+function getValuesFromBase64( encoded ){
+	const data = window.atob( encoded );
+	// Quick validation for malformed "share" query parameter
+	if( data.match(/,/g).length !== 10 ){
+		return null;
+	}
+	// Should have sorted keys based on "keysReference"
+	const values = data.split( ',' );
+	const mappedValues = keysReference.reduce( ( obj, key, index ) => ({
+		...obj,
+		[ key ]: Number( values[ index ] )
+	}), {});
+	return mappedValues;
+}
+
+/**
+ * It ill check the "share" query parameter to retrieve variables used to generate
+ * a wallpaper. If present, it will parse it and return all the decoded variables.
+ * Otherwise, it will return null
+ */
+function getDataFromUrl(){
+	const params = new URLSearchParams( window.location.search );
+	// Set from the "share" link, in "setLinks" function
+	const share = params.get( 'share' );
+	if( !share ){
+		return null;
+	}
+	return getValuesFromBase64( share );
 }
